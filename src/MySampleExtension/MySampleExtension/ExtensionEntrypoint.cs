0abac3a4b9b4bd7de658113f352838bd22b572c2
niimima <niimi@dcinc.co.jp>
2023-08-08 14:26:57 +0900

    Add MySampleExtension.sln

---
+++ src/MySampleExtension/MySampleExtension/ExtensionEntrypoint.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace MySampleExtension
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class ExtensionEntrypoint : Extension
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public ExtensionEntrypoint()
+        {
+#if DEBUG
+            // F5でデバッグ実行できないため、デバッグ実行時にはデバッガを実行する
+            // https://github.com/microsoft/VSExtensibility/issues/240
+            System.Diagnostics.Debugger.Launch();
+#endif
+        }
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+
+            // You can configure dependency injection here by adding services to the serviceCollection.
+        }
+    }
+}
